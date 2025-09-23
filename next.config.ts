128eb3d2b0fd61141e3287733bed59ac707f8227
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2025-09-23 23:17:01 +0700

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -28,15 +28,13 @@ const nextConfig: NextConfig = {
       return config;
    },
 
-   experimental: {
-      turbo: {
-         rules: {
-            "*.svg": {
-               loaders: ["@svgr/webpack"],
-               as: "*.js",
-            },
+   turbopack: {
+      rules: {
+         "*.svg": {
+            loaders: ["@svgr/webpack"],
+            as: "*.js",
          },
-      },
+      }
    },
 };
 
