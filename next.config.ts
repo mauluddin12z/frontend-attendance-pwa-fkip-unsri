971706d573fe28837deb820b709d1f110501b857
mauluddin12z <mauludindayat@gmail.com>
2025-11-21 14:31:43 +0000

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   async rewrites() {
-      return [
-         {
-            source: "/apiv1/:path*",
-            destination: `${process.env.NEXT_PUBLIC_MY_BACKEND_URL}/:path*`,
-         },
-      ];
-   },
-   // This is required to support PostHog trailing slash API requests
-   skipTrailingSlashRedirect: true,
-
    webpack(config) {
       config.module.rules.push({
          test: /\.svg$/,
@@ -38,3 +27,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
