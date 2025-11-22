c54adbfd1ae363afd30fcd624f54171d1a8f22e0
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2025-11-22 19:19:06 +0700

    update redirect default page to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,5 @@
-"use client";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import toast, { Toaster } from "react-hot-toast";
+import { redirect } from "next/navigation";
 
-export default function Page() {
-   const notify = () =>
-      toast(
-         <div className="flex items-center">
-            <div className="p-1 aspect-square rounded-full mr-4 bg-yellow-400">
-               <FontAwesomeIcon className="text-white" icon={faBell} />
-            </div>
-            Hello World!
-         </div>
-      );
-
-   return (
-      <div>
-         <button onClick={notify}>Show Toast</button>
-         {/* This renders the toast notifications */}
-         <Toaster />
-      </div>
-   );
+export default function SomePage() {
+   redirect("/login");
 }
